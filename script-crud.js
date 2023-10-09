6aac116ab760b57318e433454a6689b7fdd507d2
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2023-10-09 09:04:47 -0300

    3.1

--- script-crud.js
+++ script-crud.js
@@ -1,23 +1,27 @@
-const taskListContainer = document.querySelector('.app__section-task-list')     //
+const taskListContainer = document.querySelector('.app__section-task-list')                     //
 
-const toggleFormTaskBtn = document.querySelector('.app__button--add-task')              // Botão que abre o Formulario " + Adicionar nova tarefa"
-const cancelFormTaskBtn = document.querySelector('.app__form-footer__button--cancel')   // Botão que fecha o Formulario " X cancelar"
-const formTask = document.querySelector('.app__form-add-task')                          // O Formulario que abre dps de clicar no Botão " + Adicionar nova tarefa"
-const formLabel = document.querySelector('.app__form-label')                            // Label que esta escrito "Adicionando Tarefa"
+const toggleFormTaskBtn = document.querySelector('.app__button--add-task')                      // Botão que abre o Formulario " + Adicionar nova tarefa"
+const cancelFormTaskBtn = document.querySelector('.app__form-footer__button--cancel')           // Botão que fecha o Formulario " X cancelar"
+const formTask = document.querySelector('.app__form-add-task')                                  // O Formulario que abre dps de clicar no Botão " + Adicionar nova tarefa"
+const formLabel = document.querySelector('.app__form-label')                                    // Label que esta escrito "Adicionando Tarefa"
 
-const textarea = document.querySelector('.app__form-textarea')                          // Onde digita a tarefa
+const taskAtiveDescription = document.querySelector('.app__section-active-task-description')    // 
 
-//  Tarefas dentro de uma lista
-let tarefas = [
-    // {
-    //     descricao: 'Tarefa Concluída',
-    //     concluida: true
-    // },
-    // {
-    //     descricao: 'Tarefa Pendente',
-    //     concluida: false
-    // }
-]
+const textarea = document.querySelector('.app__form-textarea')                                  // O Campo onde digita a tarefa
+
+const localStorageTarefas = localStorage.getItem('tarefas')                                     // Pega os itens na lista de tarefas e coloca no local storage
+
+//  Verifica se no local storage existe uma lista de tarefas salvas se não tiver cria uma lista de Tarefas vazia
+let tarefas = localStorageTarefas ? JSON.parse(localStorageTarefas) :  [
+                                                                            // {
+                                                                            //     descricao: 'Tarefa Concluída',
+                                                                            //     concluida: true
+                                                                            // },
+                                                                            // {
+                                                                            //     descricao: 'Tarefa Pendente',
+                                                                            //     concluida: false
+                                                                            // }
+                                                                        ]
 
 //  Icone
 const taskIconSvg = `
@@ -29,6 +33,30 @@ const taskIconSvg = `
         fill="#01080E" />
 </svg>
 `
+
+// 
+let tarefaSelecionada = null
+let itemTarefaSelecionada = null
+
+// Função que seleciona tarefas que já existem
+const selecionaTarefa = (tarefa, elemento) => {
+
+document.querySelectorAll('.app__section-task-list-item-active').forEach(function (button) {
+    button.classList.remove('app__section-task-list-item-active')
+})
+
+if (tarefaSelecionada == tarefa) {
+    taskAtiveDescription.textContent = null
+    itemTarefaSelecionada = null
+    tarefaSelecionada = null
+    return
+}
+
+tarefaSelecionada = tarefa
+itemTarefaSelecionada = elemento
+taskAtiveDescription.textContent = tarefa.descricao
+elemento.classList.add('app__section-task-list-item-active')
+}
 
 //  Função que organiza o conteudo da tarefa para o HTML
 function createTask(tarefa) {
@@ -43,6 +71,24 @@ function createTask(tarefa) {
     paragraph.classList.add('app__section-task-list-item-description')      // <p class="app__section-task-list-item-description"></p>
 
     paragraph.textContent = tarefa.descricao                                //<p class="app__section-task-list-item-description"> tarefa.descricao </p>
+
+    const button = document.createElement('button')
+
+    li.onclick = () => {
+        selecionaTarefa(tarefa, li)
+    }
+
+    // marca a tarefa como concluída clicando no 'icone svg'
+    svgIcon.addEventListener('click', (event) => {
+        event.stopPropagation()
+        button.setAttribute('disabled', true)
+        li.classList.add('app__section-task-list-item-complete')
+    })
+
+    if(tarefa.concluida){
+        button.setAttribute('disabled', true)
+        li.classList.add('app__section-task-list-item-complete')
+    }
 
     li.appendChild(svgIcon)                                                 // <li> <svg> taskIconSvg </svg> </li>
     li.appendChild(paragraph)                                               // <li> <svg> taskIconSvg </svg>  <p> tarefa.descricao </p> </li>
@@ -78,6 +124,11 @@ cancelFormTaskBtn.addEventListener('click', () => {
     limparForm()
 })
 
+//  Salva os dados no local storage
+const updateLocalStorage = () => {
+    localStorage.setItem('tarefas', JSON.stringify(tarefas))
+}
+
 //  Captura a tarefa no "adicionando tarefa" e manda pra "lista de tarefas:"
 formTask.addEventListener('submit', (evento) => {
     evento.preventDefault()
@@ -88,6 +139,6 @@ formTask.addEventListener('submit', (evento) => {
     tarefas.push(task)
     const taskItem = createTask(task)
     taskListContainer.appendChild(taskItem)
+    updateLocalStorage()
     limparForm()
 })
-
